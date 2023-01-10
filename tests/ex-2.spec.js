1e766a6a3d60d7e3dba4d3863872b9871ca530f1
Jettapat <jettapat@techupth.com>
2023-01-10 16:08:15 +0700

    add test case

---
+++ tests/ex-2.spec.js
@@ -0,0 +1,24 @@
+//tests/[example.spec.js]
+
+// @ts-check
+const { test, expect } = require('@playwright/test');
+import path from 'path';
+test('exercise-2-test', async ({ page }) => {
+  const currentPath = path.join(process.cwd(), 'ex-2/index.html'); // change [Folder] to name of folder to test.
+
+  const url = "file:///" + currentPath
+  await page.setViewportSize({
+    width: 375, //width browser of test
+    height: 2466 // height browser of test
+  })
+  await page.goto(url)
+  expect(await page.screenshot({
+    fullPage: true,
+  })).toMatchSnapshot({
+    name: "ex-2-result.png", // ['example'] should be same file name, must include .png
+    threshold: 0.2, // you can adjust to fit each hackhour
+    maxDiffPixels: 30000, //you can adjust to fit each hackhour
+    maxDiffPixelRatio: 0.2, //you can adjust to fit each hackhour
+  },)
+
+});
